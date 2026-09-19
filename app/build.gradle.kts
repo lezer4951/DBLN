cf8d0c54fadff9e70535392fdff3c068e6d31df0
lezer4951 <a20250015@utem.edu.mx>
2026-09-19 15:46:20 -0600

    Habilita Coroutines Test en pruebas instrumentadas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,4 +86,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.room.testing)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
 }
