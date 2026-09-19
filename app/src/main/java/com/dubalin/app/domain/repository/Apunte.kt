950564dbf8b1f7868a9ccc4e29184acfb908c8d2
lezer4951 <a20250015@utem.edu.mx>
2026-09-19 15:43:52 -0600

    Elimina la ubicación anterior del modelo Apunte

--- app/src/main/java/com/dubalin/app/domain/repository/Apunte.kt
+++
@@ -1,9 +0,0 @@
-package com.dubalin.app.domain.model
-
-data class Apunte(
-    val id: Int,
-    val seccionId: Int,
-    val titulo: String,
-    val contenido: String,
-    val fechaActualizacion: Long
-)
